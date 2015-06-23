2943a76f05eb89dc21bb6451a1708f46bd992585
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-06-23 16:39:48 +0200

    Update
    Code Climate

--- leaflet.mouseCoordinate.js
+++ leaflet.mouseCoordinate.js
@@ -332,15 +332,7 @@ L.Control.mouseCoordinate  = L.Control.extend({
         return {zone: zone,band: band, x: ew2, y: nw2};
     },
     _mgr2utm: function (mgr){
-        /* Copyright (c) 2006, HELMUT H. HEIMEIER
-         Permission is hereby granted, free of charge, to any person obtaining a
-         copy of this software and associated documentation files (the "Software"),
-         to deal in the Software without restriction, including without limitation
-         the rights to use, copy, modify, merge, publish, distribute, sublicense,
-         and/or sell copies of the Software, and to permit persons to whom the
-         Software is furnished to do so, subject to the following conditions:
-         The above copyright notice and this permission notice shall be included
-         in all copies or substantial portions of the Software.*/
+        /* Copyright (c) 2006, HELMUT H. HEIMEIER */
 
         /* Die Funktion wandelt militaeische UTM Koordinaten (MGR oder
          UTMREF) in zivile UTM Koordinaten um.

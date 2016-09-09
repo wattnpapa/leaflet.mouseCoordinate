93b682870a51c4efaaa51bef41309a2f5f917712
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-09-09 11:02:01 +0200

    update dist

--- dist/leaflet.mouseCoordinate.js
+++ dist/leaflet.mouseCoordinate.js
@@ -17,6 +17,9 @@ L.Control.mouseCoordinate  = L.Control.extend({
     },
     onAdd: function(map){
         this._map = map;
+
+        if(L.Browser.touch || L.Browser.msTouch)
+            return L.DomUtil.create('div');
         
         var className = 'leaflet-control-mouseCoordinate';
         var container = this._container = L.DomUtil.create('div',className);
@@ -24,7 +27,6 @@ L.Control.mouseCoordinate  = L.Control.extend({
         this._gpsPositionContainer = L.DomUtil.create("div","gpsPos",container);
         
         map.on("mousemove", this._update, this);
-        //map.whenReady(this._update, this);
         
         return container;
     },
@@ -52,6 +54,10 @@ L.Control.mouseCoordinate  = L.Control.extend({
         if(this.options.qth){
             var qth = QTH.fromLatLng(gps);
             content += "<tr><td>QTH</td><td colspan='2'>"+qth+"</td></tr>";
+        }
+        if(this.options.nac){
+            var nac = NAC.fromLatLng(gps);
+            content += "<tr><td>NAC</td><td colspan='2'>"+nac.y+" "+ nac.x +"</td></tr>";
         }
             
         content += "</table>";
@@ -102,6 +108,74 @@ L.control.mouseCoordinate = function (options) {
     return new L.Control.mouseCoordinate(options);
 };
 ;/**
+ * Created by Johannes Rudolph <johannes.rudolph@gmx.com> on 01.09.2016.
+ */
+
+/**
+ *
+ * @type {{fromLatLng: NAC.fromLatLng, _nac2Letter: NAC._nac2Letter}}
+ */
+var NAC = {
+
+    /**
+     *
+     * @param {{lat: number, lng: number}}
+     * @returns {string}
+     */
+    fromLatLng: function(latlng) {
+
+        var lat = latlng.lat;
+        var lon = latlng.lng;
+        var x = [];
+        var y = [];
+        var xy = [];
+        xy.x = '';
+        xy.y = '';
+        if (lon >= -180 && lon <= 180) {
+            var xlon = (lon + 180) / 360;
+            x[0] = parseInt(xlon * 30);
+            x[1] = parseInt((xlon * 30 - x[0]) * 30);
+            x[2] = parseInt(((xlon * 30 - x[0]) * 30 - x[1]) * 30);
+            x[3] = parseInt((((xlon * 30 - x[0]) * 30 - x[1]) * 30 - x[2]) * 30);
+            x[4] = parseInt(((((xlon * 30 - x[0]) * 30 - x[1]) * 30 - x[2]) * 30 - x[3]) * 30);
+            x[5] = parseInt((((((xlon * 30 - x[0]) * 30 - x[1]) * 30 - x[2]) * 30 - x[3]) * 30 - x[4]) * 30);
+        } else {
+            x[0] = 0;
+        }
+        if (lat >= -90 && lat <= 90) {
+            var ylat = (lat + 90) / 180;
+            y[0] = parseInt(ylat * 30);
+            y[1] = parseInt((ylat * 30 - y[0]) * 30);
+            y[2] = parseInt(((ylat * 30 - y[0]) * 30 - y[1]) * 30);
+            y[3] = parseInt((((ylat * 30 - y[0]) * 30 - y[1]) * 30 - y[2]) * 30);
+            y[4] = parseInt(((((ylat * 30 - y[0]) * 30 - y[1]) * 30 - y[2]) * 30 - y[3]) * 30);
+            y[5] = parseInt((((((ylat * 30 - y[0]) * 30 - y[1]) * 30 - y[2]) * 30 - y[3]) * 30 - y[4]) * 30);
+        } else {
+            y[0] = 0;
+        }
+        for (var i = 0; i < x.length; i++) {
+            xy.x += this._nac2Letter(x[i]);
+        }
+        for (i = 0; i < y.length; i++) {
+            xy.y += this._nac2Letter(y[i]);
+        }
+        return xy;
+    },
+
+    /**
+     *
+     * @param number
+     * @returns {string}
+     * @private
+     */
+    _nac2Letter: function(number){
+        var nac_letters = "0123456789BCDFGHJKLMNPQRSTVWXZ";
+        if(!isNaN(number) && number < 30)
+            return nac_letters.substr(number,1);
+        else return 0;
+    }
+
+};;/**
  * Created by Johannes Rudolph <johannes.rudolph@gmx.com> on 01.09.2016.
  */
 
